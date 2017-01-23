389e2adeffc07cc131ac9907856695dbf754ee46
fedodd <fedodd.s@gmail.com>
2017-01-23 09:25:15 +0300

    refactor_2

---
+++ javascript/refactor_2.js
@@ -0,0 +1,19 @@
+//я бы переписал задачу с одним условием, завернув в цикл повторяющиеся операции
+var vote = 0;
+
+function drawRating(vote) {
+  var star = Math.ceil(vote / 20);
+  var emptyStar = '☆';
+  var earnStar = '★';
+  var rate = '★';
+// Ставим закрашенные звезды согласно голосам, 
+//затем выставляем пустые, пока не будет 5 штук.
+  for (i=2; i <=5; i++) {
+    if ( star >= i) {
+    rate +=earnStar;
+    } else {
+    rate +=emptyStar;
+    }
+  }
+  return rate
+}
